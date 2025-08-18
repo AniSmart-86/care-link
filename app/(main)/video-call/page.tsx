a19bb81591400aacb6df153dd48d924f9f2c34b6
AniSmart-86 <anismart124@gmail.com>
2025-08-18 16:17:54 +0100

    init

--- app/(main)/video-call/page.tsx
+++ app/(main)/video-call/page.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import VideoCall from './_component/videoCall';
-import { useSearchParams } from 'next/navigation';
+"use client";
 
-const VideoCallPage = () => {
+import { useSearchParams } from "next/navigation";
+import VideoCall from "./_component/videoCall";
+
+export default function VideoCallPage() {
   const searchParams = useSearchParams();
 
-  const sessionId = searchParams.get('sessionId') ?? "";
-  const token = searchParams.get('token') ?? "";
+  const sessionId = searchParams.get("sessionId") ?? "";
+  const token = searchParams.get("token") ?? "";
 
   return (
     <div>
       <VideoCall sessionId={sessionId} token={token} />
     </div>
-  )
+  );
 }
-
-export default VideoCallPage;
